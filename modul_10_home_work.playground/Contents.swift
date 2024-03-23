8be37d1654506207faaee9c3252ab45c25d09dad
shadon <in-con23@yandex.ru>
2024-03-23 11:28:07 +0500

    tasks 2

--- modul_10_home_work.playground/Contents.swift
+++ modul_10_home_work.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-//Задание 1
+// MARK: Задание 1
 //Объявите три сущности, которые реализует предоставленный вам протокол. После этого для всех сущностей вызовите все методы и свойства, объявленные в протоколе.
 
 //Протокол:
@@ -71,3 +71,70 @@ calorieCount.forEach { item in
 
 
 
+
+
+// MARK: Задание 2
+
+//Поработайте с протоколом Equatable, который нужен для сравнения сущностей.
+
+//Вам предоставлены структура и класс, которые нужно сравнить. Добавьте в объявление этих сущностей протокол Equatable. При необходимости реализуйте недостающий метод.
+
+//Структура:
+
+//enum BalanceType {
+//    case positive, negative, neutral
+//}
+
+//struct Balance {
+//    let type: BalanceType
+//    let amount: Int
+//}
+
+
+//Класс:
+
+//class BalanceObject {
+//    var amount: Int = 0
+//}
+
+//Должны выполняться следующие условия:
+
+//Структура и класс реализуют протокол Equatable.
+//Проведена проверка, что протокол реализуется. Для этого сравните экземпляры этих сущностей с помощью оператора ‘==’.
+
+
+// Структура:
+
+enum BalanceType {
+    case positive, negative, neutral
+}
+
+struct Balance: Equatable {
+    let type: BalanceType
+    let amount: Int
+}
+ 
+ // Класс:
+
+class BalanceObject: Equatable {
+    static func == (lhs: BalanceObject, rhs: BalanceObject) -> Bool {
+        return lhs.amount == rhs.amount
+    }
+
+    var amount: Int = 0
+}
+
+var balance0 = Balance(type: .positive, amount: 5)
+var balance = Balance(type: .positive, amount: 5)
+var balance2 = Balance(type: .negative, amount: 3)
+var balance3 = Balance(type: .neutral, amount: 4)
+//var balance4 = BalanceObject()
+
+balance == balance2
+balance == balance3
+balance2 == balance3
+balance0 == balance
+
+
+// balance == balance4
+// Binary operator '==' cannot be applied to operands of type 'Balance' and 'BalanceObject'
