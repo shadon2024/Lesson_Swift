f2fb37d7f1825d4006b688f460839295a1bc7259
shadon <in-con23@yandex.ru>
2024-03-27 16:36:51 +0500

    addProject

---
+++ modul_12/m_12/m_12/m_12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m_12
+//
+//  Created by Admin on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
