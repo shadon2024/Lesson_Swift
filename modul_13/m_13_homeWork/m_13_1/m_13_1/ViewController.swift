1ef8829716751e9675a7d2c549046945c2874a3d
shadon <in-con23@yandex.ru>
2024-03-29 12:06:18 +0500

    Tab Bar

---
+++ modul_13/m_13_homeWork/m_13_1/m_13_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m_13_1
+//
+//  Created by Admin on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
