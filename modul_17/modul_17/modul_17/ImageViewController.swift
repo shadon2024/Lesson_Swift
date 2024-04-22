dd1510ac3fc72db491a845c4b67bc72186a393f0
shadon2024 <in-con23@yandex.ru>
2024-04-22 15:43:17 +0500

    add framework SnapKit and add new fail imageViewController

---
+++ modul_17/modul_17/modul_17/ImageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ImageViewController.swift
+//  modul_17
+//
+//  Created by Admin on 22/04/24.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ modul_17/modul_17/modul_17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  modul_17
+//
+//  Created by Admin on 22/04/24.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
